ce4641405bfd7a9613c6bec27f55d0d8abab307e
Ashish Deshmukh <ashishdeshmukh1843@gmail.com>
2025-12-14 16:55:43 +0530

    new changes

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Connect MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/contactForm", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
+ .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 // Schema
