de21d23ade24f2e44c9e50f2ff6995ca378d6cd0
Ashish Deshmukh <ashishdeshmukh1843@gmail.com>
2025-12-15 12:27:14 +0530

    new changes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Connect MongoDB
 mongoose.connect(process.env.MONGO_URI)
